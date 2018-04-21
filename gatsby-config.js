315ffc129472d649c09816c7011e771eadee2569
Gabriel <gabrielperales@gmail.com>
2018-04-21 18:39:35 +0200

    added favicon and offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,38 @@ module.exports = {
   siteMetadata: {
     title: 'Wombak - JavaScript experts',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/images/wombak.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: true,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Wombak. Building better world wide web',
+        short_name: 'Wombak',
+        start_url: '/',
+        background_color: '#000000',
+        theme_color: '#F5E05B',
+        display: 'minimal-ui',
+        icon: 'src/images/wombak.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
 }
